93fc356b38c15a08ab5ae50a504037d53db5369f
AlexeuTsukanov <alexeuel@gmail.com>
2017-10-30 01:15:28 +0200

    Исправил ошибку компиляции.

--- src/com/NumberInput.java
+++ src/com/NumberInput.java
@@ -7,11 +7,10 @@ import java.util.regex.Pattern;
 public class NumberInput {
     private int[] numbers = new int[4];
 
-    public NumberInput(){
-        for (int i = 0; i < 4; i++){
+    public NumberInput() {
             numbers = testInt();
-        }
     }
+
     public boolean checkNumbers() {
         for (int i = 0; i < numbers.length; i++) {
             for (int j = numbers.length - 1; j > i; j--) {
@@ -28,29 +27,27 @@ public class NumberInput {
         Scanner scanner = new Scanner(System.in);
         int timeNumb;
         String tNInt;
-        String stringPattern = "\\d";
+        String stringPattern = "[\\d]";
         System.out.println("Введите число!");
         int[] numb = new int[4];
-        while (true) {
-            if (scanner.hasNextInt()) {
-                timeNumb = scanner.nextInt();
-                if (timeNumb >= 1000 & timeNumb < 9999) {
-                    break;
-                }
-                else {
-                    System.out.println("Введенное число не соответствует условию.");
+        if (scanner.hasNextInt()) {
+            timeNumb = scanner.nextInt();
+            if (timeNumb >= 1000 & timeNumb < 9999) {
+                tNInt = Integer.toString(timeNumb);
+                Pattern pattern = Pattern.compile(stringPattern);
+                Matcher matcher = pattern.matcher(tNInt);
+                int i = 0;
+                while (matcher.find()) {
+                    String s = matcher.group();
+                    numb[i] = Integer.valueOf(s);
+                    i++;
                 }
             } else {
-                System.out.println("Вы ввели не целое число!");
-                scanner.next();
+                System.out.println("Введенное число не соответствует условию.");
             }
-        }
-        tNInt = Integer.toString(timeNumb);
-        Pattern pattern = Pattern.compile(stringPattern);
-        Matcher matcher = pattern.matcher(tNInt);
-        for (int i = 0; i < 4; i++){
-            String s = matcher.group();
-            numb[i] = Integer.valueOf(s);
+        } else {
+            System.out.println("Вы ввели не целое число!");
+            scanner.next();
         }
         return numb;
     }
